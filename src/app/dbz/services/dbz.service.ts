fbb13f5080c68583e4d6b551adf9600541232033
Rafael Botía <rafabotiaordaz@gmail.com>
2023-05-31 11:41:55 +0200

    primer commit

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+import { v4 as uuid }  from 'uuid';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbzService {
+
+  public characters: Character[] = [{
+    id: uuid(),
+    name: 'Krillin',
+    power: 1000
+  }, {
+    id: uuid(),
+    name: 'Goku',
+    power: 9500
+  }, {
+    id: uuid(),
+    name: 'Vegeta',
+    power: 7500
+  }];
+
+  // onNewCharacter( character: Character): void {
+  //   console.log("'Main Page");
+  //   console.log("Character", character)
+
+  //   // const newCharacter: Character = {
+  //   //   id: uuid(),
+  //   //   name: character.name,
+  //   //   power: character.power
+  //   // }
+
+  //   const newCharacter: Character = { id: uuid(), ...character};
+
+  //   this.characters.push(newCharacter);
+  // }
+
+  addwCharacter( character: Character): void {
+    console.log("'Main Page");
+    console.log("Character", character)
+
+    // const newCharacter: Character = {
+    //   id: uuid(),
+    //   name: character.name,
+    //   power: character.power
+    // }
+
+    const newCharacter: Character = { id: uuid(), ...character};
+
+    this.characters.push(newCharacter);
+  }
+
+  // onDeleteCharacter( index: number): void {
+  //   this.characters.splice(index,1);
+  // }
+
+  deleteCharacterById( id: string) {
+    this.characters = this.characters.filter( character => character.id !== id );
+  }
+
+
+}
